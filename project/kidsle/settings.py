f18d24a6df7a6b4a800ada550c9e79abaf15062f
Max Brauer <max@transcode.de>
2014-05-15 18:18:13 +0200

    added grappelli to project

--- project/kidsle/settings.py
+++ project/kidsle/settings.py
@@ -30,12 +30,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party apps
+    'grappelli',
+    'django.contrib.admin',
 )
 
 MIDDLEWARE_CLASSES = (
