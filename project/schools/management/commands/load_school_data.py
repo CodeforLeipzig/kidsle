667ab9b2ecff59924e9c61d9db5e97f5885d754c
Max Brauer <max@transcode.de>
2014-05-20 21:33:31 +0200

    added management command to load school data

---
+++ project/schools/management/commands/load_school_data.py
@@ -0,0 +1,43 @@
+import os
+import json
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from schools.models import School
+
+
+SCHOOL_DATA_DIR = os.path.join(settings.BASE_DIR, 'schools', 'data')
+
+SCHOOL_DATA_FILES = [
+    (School.ELEMENTARY_SCHOOL, os.path.join(SCHOOL_DATA_DIR, 'grundschule.geo.json')),
+    (School.GRAMMAR_SCHOOL, os.path.join(SCHOOL_DATA_DIR, 'gymnasium.geo.json'))
+]
+
+
+class Command(BaseCommand):
+    help = 'Parses the geo json files, located in schools/data/'
+
+    def handle(self, *args, **options):
+        for school_type, json_file in SCHOOL_DATA_FILES:
+            with open(json_file) as f:
+                data = json.loads(f.read())
+                for school_information in data['features']:
+                    name = school_information['properties']['name']
+                    school_info, adress_info = school_information['properties']['popupContent'].split('<br />')
+                    post_code = adress_info.split(' ')[-2]
+                    street = ' '.join(adress_info.split(' ')[:-2])
+                    town = adress_info.split(' ')[-1]
+                    latitude = school_information["geometry"]["coordinates"][1]
+                    longitude = school_information["geometry"]["coordinates"][0]
+                    obj, created = School.objects.get_or_create(
+                        name=name,
+                        post_code=post_code,
+                        street=street,
+                        town=town,
+                        school_type=school_type,
+                        latitude=latitude,
+                        longitude=longitude
+                    )
+                    if created:
+                        self.stdout.write('Created School %s' % name)
