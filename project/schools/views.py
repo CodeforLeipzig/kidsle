97354066e1976032755076a59b2d5cfdf2c93438
Max Brauer <max@transcode.de>
2014-05-20 22:19:03 +0200

    added api views for school

--- project/schools/views.py
+++ project/schools/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import School
+from .serializers import SchoolSerializer
+
+
+class SchoolViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = School.objects.all()
+    serializer_class = SchoolSerializer
