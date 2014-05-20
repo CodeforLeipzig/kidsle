a97ed6478a7a19dc6472eb688de9efd591478a6b
Max Brauer <max@transcode.de>
2014-05-20 21:32:22 +0200

    added admin for school

--- project/schools/admin.py
+++ project/schools/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import School
+
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('name', 'school_type', 'street', 'post_code', 'town')
+    list_filter = ('school_type', 'post_code')
+    search_fields = ('name', 'street')
+
+
+admin.site.register(School, SchoolAdmin)
