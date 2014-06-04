c18f3f69174a1d3f073dad10ac1ddf8916b4c56e
rmuhl <r.muhl@gmx.net>
2014-06-04 15:00:54 +0200

    added model of playgrounds

--- project/playgrounds/models.py
+++ project/playgrounds/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+'''
+Spielplätze in Kategorien zusammenfassen? Bsp. nach Alter anhand der gaming devices/ des equipments
+PLZ aus Koordinaten mappen?
+'''
+
+
+class Playgrounds(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Name')
+    district = models.CharField(max_length=100, verbose_name='Stadtteil')
+    location = models.CharField(max_length=100, verbose_name='Gegend')
+    town = models.CharField(max_length=100, verbose_name='Stadt')
+    gaming_devices = models.CharField(max_length=200, verbose_name='Spielgeräte')
+    equipment = models.CharField(max_length=200, verbose_name='Ausstattung')
+    line = models.SmallIntegerField(max_length=100, verbose_name='Linie')
+    stop = models.SmallIntegerField(max_length=100, verbose_name='Haltestelle')
+    latitude = models.CharField(max_length=100, verbose_name='')
+    longitude = models.CharField(max_length=100, verbose_name='')
