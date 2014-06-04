49949760116f62743067b3db813953c7ab3b41e6
rmuhl <r.muhl@gmx.net>
2014-06-04 23:15:23 +0200

    added json2geo.json script and resulting geo.json formated playgrounds

--- project/schools/serializers.py
+++ project/schools/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 
 from .models import School
 
-class SchoolSerializer(serializers.HyperlinkedModelSerializer):
+
+class PlayroundSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = School
         fields = ('name', 'post_code', 'street', 'town', 'school_type', 'latitude', 'longitude')
