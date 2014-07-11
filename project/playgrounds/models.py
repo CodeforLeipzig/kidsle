29e23c63e0750e84de357ce20e6071c9b6f2129e
rmuhl <r.muhl@gmx.net>
2014-07-11 23:41:15 +0200

    - deleted json2geojson - added model for playgrounds

--- project/playgrounds/models.py
+++ project/playgrounds/models.py
@@ -2,12 +2,21 @@
 from django.db import models
 
 
+class GamingDevice(models.Model):
+    '''helper class for different gaming devices'''
+    def __init__(self, title):
+        self.title = title
+
+    title = models.CharField(max_length=100)
+
+
 class Playground(models.Model):
     name = models.CharField(max_length=100, verbose_name='Name')
     district = models.CharField(max_length=100, verbose_name='Stadtteil')
     location = models.CharField(max_length=100, verbose_name='Gegend')
     town = models.CharField(max_length=100, verbose_name='Stadt')
-    gaming_devices = models.CharField(max_length=200, verbose_name='Spielgeräte')
+    gaming_devices = models.ManyToManyField(GamingDevice,
+        through='Spielgeräte')
     equipment = models.CharField(max_length=200, verbose_name='Ausstattung')
     lines = models.CharField(max_length=100, verbose_name='Linien')
     stops = models.CharField(max_length=100, verbose_name='Haltestellen')
