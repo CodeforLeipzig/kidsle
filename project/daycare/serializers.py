8b125262fb5d20a5c81588376d5a1d9119fd6053
Max Brauer <max@transcode.de>
2014-08-05 23:02:14 +0200

    Updated Serializers

--- project/daycare/serializers.py
+++ project/daycare/serializers.py
@@ -2,7 +2,16 @@ from rest_framework import serializers
 
 from .models import DayCareCenter
 
+
+class DaycareTypeFieldSerializer(serializers.Field):
+    def to_native(self, data):
+        care_types_dict = dict(DayCareCenter.CARE_TYPES)
+        return care_types_dict.get(data, 'Unbekannt')
+
+
 class DaycareSerializer(serializers.HyperlinkedModelSerializer):
+    daycare_type = DaycareTypeFieldSerializer()
+
     class Meta:
         model = DayCareCenter
         fields = ('name', 'daycare_type', 'zipcode', 'address', 'latitude', 'longitude', 'district')
--- project/schools/serializers.py
+++ project/schools/serializers.py
@@ -2,7 +2,15 @@ from rest_framework import serializers
 
 from .models import School
 
+
+class SchoolTypeFieldSerializer(serializers.Field):
+    def to_native(self, data):
+        school_types_dict = dict(School.SCHOOL_TYPES)
+        return school_types_dict.get(data, 'Unbekannt')
+
+
 class SchoolSerializer(serializers.HyperlinkedModelSerializer):
+    school_type = SchoolTypeFieldSerializer()
     class Meta:
         model = School
         fields = ('name', 'post_code', 'street', 'town', 'school_type', 'latitude', 'longitude')
