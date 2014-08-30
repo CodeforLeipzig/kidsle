db9d8be9d775a9f2f8c11d2d38844dba32682ced
rmuhl <r.muhl@gmx.net>
2014-08-30 23:46:29 +0200

    Removed unnecessary models, added unicode compatibility

--- project/playgrounds/models.py
+++ project/playgrounds/models.py
@@ -1,35 +1,30 @@
 # encoding: utf-8
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class GamingDevice(models.Model):
     title = models.CharField(max_length=100, verbose_name='Spielgeräte')
 
+    def __str__(self):
+        return self.title
 
+@python_2_unicode_compatible
 class Equipment(models.Model):
     title = models.CharField(max_length=200, verbose_name='Ausstattung')
 
+    def __str__(self):
+        return self.title
 
 class Playground(models.Model):
     name = models.CharField(max_length=100, verbose_name='Name')
     district = models.CharField(max_length=100, verbose_name='Stadtteil')
     location = models.CharField(max_length=100, verbose_name='Gegend')
     town = models.CharField(max_length=100, verbose_name='Stadt')
-    gaming_devices = models.ManyToManyField(GamingDevice, through='GamingDevicePlaygroundM2M')
-    equipment = models.ManyToManyField(Equipment, through='EquipmentPlaygroundM2M')
+    gaming_devices = models.ManyToManyField(GamingDevice)
+    equipment = models.ManyToManyField(Equipment)
     lines = models.CharField(max_length=200, verbose_name='öffentliche Verkehrsmittel')
     stops = models.CharField(max_length=100, verbose_name='Haltestellen')
     latitude = models.CharField(max_length=100, verbose_name='')
     longitude = models.CharField(max_length=100, verbose_name='')
-
-
-class GamingDevicePlaygroundM2M(models.Model):
-    '''helper class for different gaming devices'''
-    device = models.ForeignKey(GamingDevice)
-    playground = models.ForeignKey(Playground)
-
-
-class EquipmentPlaygroundM2M(models.Model):
-    '''helper class for different equipment'''
-    equipment = models.ForeignKey(Equipment)
-    playground = models.ForeignKey(Playground)
